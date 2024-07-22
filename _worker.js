69ab415450567df7b02cd400de6a182ba7a35f40
xiaoqian681 <79614802+xiaoqian681@users.noreply.github.com>
2024-07-22 11:31:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'a2e614ed-ab06-4b84-a6c5-55915b2d3365';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
